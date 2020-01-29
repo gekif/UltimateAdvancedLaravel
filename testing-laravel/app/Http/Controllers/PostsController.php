29b67b4828b621c17ddea7a32b76e7e1476a431a
Gekif <dzulfikar.maulana@gmail.com>
2020-01-29 22:14:45 +0700

    03 TESTING IN LARAVEL AND TEST DRIVEN DEVELOPMENT: 017 Refactoring to controllers (fix)

--- testing-laravel/app/Http/Controllers/PostsController.php
+++ testing-laravel/app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -13,7 +14,7 @@ class PostsController extends Controller
      */
     public function index($id)
     {
-        return view('posts')->withPost(Post::find($id));
+        return view('post')->withPost(Post::find($id));
     }
 
     /**
