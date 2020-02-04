b86b9cb2afe959e979c31ea765580e04b226241a
Gekif <dzulfikar.maulana@gmail.com>
2020-02-04 08:51:38 +0700

    04 Project Build a LARACASTS CLONE WITH LARAVEL AND VUE.JS: 032 The login modal with Vuejs

--- gekifcast/resources/views/layouts/app.blade.php
+++ gekifcast/resources/views/layouts/app.blade.php
@@ -1,80 +1,84 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/core.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/thesaas.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <title>GekifCast</title>
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="{{ asset('assets/img/apple-touch-icon.png') }}">
+    <link rel="icon" href="{{ asset('assets/img/favicon.png') }}">
+
+    @yield('scripts')
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+<div id="app">
+    <!-- Topbar -->
+    <nav class="topbar topbar-inverse topbar-expand-md topbar-sticky">
+        <div class="container">
+            <div class="topbar-left">
+                <button class="topbar-toggler">&#9776;</button>
+                <a class="topbar-brand" href="/" style="color: white;">
+                    GekifCast
+                </a>
+            </div>
+            <div class="topbar-right">
+                <ul class="topbar-nav nav">
+                    <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                    @if(auth()->check())
+                        <li class="nav-item">
+                            Hello {{ auth()->user()->name }}
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="javascript:;" data-toggle="modal" data-target="#loginModal">Login</a>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- END Topbar -->
+    <!-- Header -->
+@yield('header')
+<!-- END Header -->
+    <!-- Main container -->
+    <main class="main-content">
+        @yield('content')
+    </main>
+    <!-- END Main container -->
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+    @if(!auth()->check())
+        <vue-login></vue-login>
+@endif
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
+<!-- Footer -->
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row gap-y justify-content-center">
+                <div class="col-12 col-lg-6">
+                    <ul class="nav nav-primary nav-hero">
+                        <li class="nav-item hidden-sm-down">
+                            <a class="nav-link" href="/">GekifCast</a>
+                        </li>
                     </ul>
                 </div>
             </div>
-        </nav>
+        </div>
+    </footer>
+    <!-- END Footer -->
+</div>
 
-        @yield('content')
-    </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+<!-- Scripts -->
+<script src="{{ asset('assets/js/core.min.js') }}"></script>
+<script src="{{ asset('assets/js/thesaas.min.js') }}"></script>
+<script src="{{ asset('assets/js/script.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
