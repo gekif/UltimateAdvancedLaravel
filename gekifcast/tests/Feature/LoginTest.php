7cde9687b6578e7fd04cd5285566995a489effe9
Gekif <dzulfikar.maulana@gmail.com>
2020-02-11 14:08:48 +0700

    04 Project Build a LARACASTS CLONE WITH LARAVEL AND VUE.JS: 040 Writing tests for successful login

--- gekifcast/tests/Feature/LoginTest.php
+++ gekifcast/tests/Feature/LoginTest.php
@@ -11,6 +11,22 @@ class LoginTest extends TestCase
 {
     use RefreshDatabase;
 
+
+    public function test_correct_response_after_user_logs_in()
+    {
+        $user = factory(User::class)->create();
+
+        $this->postJson('/login', [
+            'email' => $user->email,
+            'password' => 'secret',
+        ])->assertstatus(200)
+            ->assertJson([
+                'status' => 'ok'
+            ])
+            ->assertSessionHas('success', 'Successfully logged in');
+    }
+
+
     public function test_a_user_receives_correct_message_when_passing_in_wrong_credentials()
     {
         $user = factory(User::class)->create();
@@ -22,6 +38,7 @@ class LoginTest extends TestCase
             ->assertJson([
                 'message' => 'These credentials do not match our records.'
             ]);
-
     }
+
+
 }
